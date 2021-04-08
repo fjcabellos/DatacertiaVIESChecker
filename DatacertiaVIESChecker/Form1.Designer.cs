d36657b39cca616b790717f0614a94e51668041b
fjcabellos <f.j.cabellos@gmail.com>
2021-04-09 00:49:22 +0200

    First commit

---
+++ DatacertiaVIESChecker/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace DatacertiaVIESChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.Location = new System.Drawing.Point(60, 109);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(104, 13);
+            this.linkLabel.TabIndex = 0;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "www.datacertia.com";
+            this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Comprobar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(63, 36);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(149, 20);
+            this.textBox.TabIndex = 2;
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "AT",
+            "BE",
+            "BG",
+            "CY",
+            "CZ",
+            "DE",
+            "DK",
+            "EE",
+            "EL",
+            "ES",
+            "FI",
+            "FR",
+            "HR",
+            "HU",
+            "IE",
+            "IT",
+            "LT",
+            "LU",
+            "LV"});
+            this.comboBox.Location = new System.Drawing.Point(63, 9);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(149, 21);
+            this.comboBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "País";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NIF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 131);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(240, 170);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(240, 170);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "datacertia VIES Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DatacertiaVIESChecker/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatacertiaVIESChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.datacertia.com/?utm_source=software&utm_medium=linklabel&utm_campaign=vieschecker");
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _ = Cursors.WaitCursor;
+            string mensaje = "";
+            bool resultado = false;
+            string soap0 =
+                        @"<?xml version=""1.0"" encoding=""utf-8""?>
+                        <soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:urn=""urn:ec.europa.eu:taxud:vies:services:checkVat:types"">
+                       <soapenv:Header/>
+                       <soapenv:Body>
+                          <urn:checkVat>
+                             <urn:countryCode>";
+            string pais = comboBox.SelectedItem.ToString();
+            string soap2 = @"</urn:countryCode>
+                             <urn:vatNumber>";
+            string nif = textBox.Text;
+            string soap3 = @"</urn:vatNumber>
+                          </urn:checkVat>
+                       </soapenv:Body>
+                    </soapenv:Envelope>";
+
+            string[] soap = { soap0, pais, soap2, nif, soap3 };
+
+
+            if (nif.Length == 0)
+            {
+                mensaje = "Debes introducir un NIF.";
+                MessageBox.Show(mensaje);
+                _ = Cursors.Default;
+                return;
+            }
+            if (nif.Contains("-"))
+            {
+                mensaje = "Debes introducir el NIF sin guiones";
+                MessageBox.Show(mensaje);
+                _ = Cursors.Default;
+                return;
+            }
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://ec.europa.eu/taxation_customs/vies/services/checkVatService");
+            req.ContentType = "text/xml;charset=\"utf-8\"";
+            req.Accept = "text/xml";
+            req.Method = "POST";
+
+            using (Stream stm = req.GetRequestStream())
+            {
+                using (StreamWriter stmw = new StreamWriter(stm))
+                {
+                    stmw.Write(string.Concat(soap));
+                }
+            }
+
+            WebResponse response = req.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+            StreamReader readStream = new StreamReader(responseStream, encode);
+            Char[] read = new Char[256];
+            int count = readStream.Read(read, 0, 256);
+            while (count > 0)
+            {
+                String str = new String(read, 0, count);
+                Console.Write(str);
+                count = readStream.Read(read, 0, 256);
+                if (str.Contains("<valid>true</valid>")) resultado = true;
+            }
+            readStream.Close();
+
+
+
+            if (resultado) mensaje = "El NIF es válido.";
+            else mensaje = "El NIF no es válido.";
+
+            MessageBox.Show(mensaje);
+            _ = Cursors.Default;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            comboBox.SelectedItem = "ES";
+        }
+    }
+}
